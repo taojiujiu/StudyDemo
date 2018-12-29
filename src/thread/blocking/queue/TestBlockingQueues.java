1648dd690fa5f0f5c0c34251f762b664d4f83aca
桃九酒 <tao9jiu@gmail.com>
2018-12-29 17:14:05 +0800

    Add blocking quenes

---
+++ src/thread/blocking/queue/TestBlockingQueues.java
@@ -0,0 +1,13 @@
+package src.thread.blocking.queue;
+
+import java.util.concurrent.BlockingQueue;
+
+public class TestBlockingQueues {
+    static class LiftOffRunner implements Runnable{
+        private BlockingQueue<LiftOff> rockets;
+        @Override
+        public void run() {
+
+        }
+    }
+}
